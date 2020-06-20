240b0a224dbe776b844d316d466ef25e8a42a8e8
Andrew Lingenfelter <andrew.lingenfelter@outlook.com>
2020-06-20 00:28:49 -0400

    started variables

---
+++ notes.rb
@@ -0,0 +1,10 @@
+# Notes
+
+# Basically everything is an object
+# even primitive types
+# objects have attributes and methods
+
+# variables
+# convention is snake case
+sample_variable = 1
+puts sample_variable
