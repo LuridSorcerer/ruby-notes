316f3171ea667a64785145c482f74317461f4350
Andrew Lingenfelter <luridsorcerer@gmail.com>
2020-06-20 09:24:32 -0400

    added numbers, integers, floats

--- notes.rb
+++ notes.rb
@@ -5,7 +5,7 @@
 # variables
 # Naming convention is snake case.
 # They are not objects, but they refrence objects. 
-sample_variable = 1
+sample_variable = 2
 
 # scope indicators
 # $global_variable
@@ -13,4 +13,23 @@ sample_variable = 1
 # @instance_variable
 # local_variable
 
+# numbers
+# + - * / work as expected
+# integer divison truncates
+# will convert to float if a float is involved
+# ** = exponent
+puts sample_variable ** 8
 
+# integers
+puts 8.class
+puts 8.next
+puts -8.abs
+puts 8.to_f
+
+# floats
+sample_float = 0.6
+puts sample_float.ceil
+puts sample_float.floor
+puts sample_float.round
+puts (sample_float.to_i).class
+puts sample_float.class
